b8b3da3d8ed4069108f192067d405799e7bbad95
Aaditya Shah <aadishah1020@gmail.com>
2025-03-16 01:38:03 -0400

    Improved the overall UI

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 let bar = document.querySelector("#mobile-menu");
 let nav = document.querySelector("#nav-links");
+console.log(window.innerWidth)
 
+function checkWidth() {
+    if("ontouchstart" in document || navigator.maxTouchPoints > 0 || window.innerWidth < 786) {
+        nav.classList.add('hide');
+    } else {
+        nav.classList.remove('hide');
+    }
+}
 bar.addEventListener('click', () => {
     console.log("clicked");
     nav.classList.toggle('hide');
-})
+});
+
+checkWidth();
+window.addEventListener('resize', checkWidth);
