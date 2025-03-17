f5c8eedad37187f516aa600a78ee7014188bad87
Aaditya Shah <aadishah1020@gmail.com>
2025-03-16 20:38:37 -0400

    Improved the sign-up page and Reorganized folder structure

---
+++ assets/js/signUp.js
@@ -0,0 +1,17 @@
+let intro = document.querySelector('.intro-container');
+
+setTimeout(() => {
+    intro.classList.add('hide');
+}, 1000);
+
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
